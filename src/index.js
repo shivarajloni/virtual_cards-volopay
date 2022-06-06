7ec3f026caaf939b09ea0e38ffd9543dd90e8198
shivarajloni <shivarajbloni@gmail.com>
2022-06-07 00:08:53 +0530

    initial commit testing app

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {BrowserRouter as Router} from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
+
+function Home() {
+  return (
+    <div>
+      <h1>test Home Page</h1>
+    </div>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
